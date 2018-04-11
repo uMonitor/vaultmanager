dec611ba87329b7803e832d65b0786bb9db7a063
Bhaal22 <muller.john@gmail.com>
2018-04-11 18:20:07 +0200

    initial commit for vaultmanager command line tool

---
+++ test/vaultmanager/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using vaultsharp;
+using vaultsharp.native;
+
+namespace vaultmanager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var credentialTarget = "MicrosoftOffice16_Data:SSPI:mapitestuser@gsx.com";
+
+            IntPtr creds;
+
+            //Class1.EnumerateCredentials();
+
+            //var success = CredentialManagerWrapper.CredRead(credentialTarget, CredentialType.CRED_TYPE_GENERIC, 0, out creds);
+            //var c = (NativeCredential)Marshal.PtrToStructure(creds, typeof(NativeCredential));
+
+            //string applicationName = Marshal.PtrToStringUni(c.TargetName);
+            //string userName = Marshal.PtrToStringUni(c.UserName);
+            //string secret = null;
+            //if (c.CredentialBlob != IntPtr.Zero)
+            //{
+            //    secret = Marshal.PtrToStringUni(c.CredentialBlob, (int)c.CredentialBlobSize / 2);
+            //}
+
+
+            WindowsCredentialManager.WriteCredentials(credentialTarget, "", "plop");
+        }
+    }
+}
